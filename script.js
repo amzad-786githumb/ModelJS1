732d1a8eddaab48227e952b1a290630a7fb73e2d
amzad-786githumb <73783489+amzad-786githumb@users.noreply.github.com>
2023-11-02 16:57:21 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+"use struct";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnOpenModal = document.querySelectorAll(".show-modal");
+console.log(btnOpenModal);
+
+const openModel = function () {
+  console.log("Button clicked ");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener("click", openModel);
+}
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  console.log(e.key);
+
+  if (e.key === "Escape" && !modal.classList.contains("hideen")) {
+    closeModal();
+  }
+});
